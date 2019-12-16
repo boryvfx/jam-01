08a705d6eeface1b9bdae85e9b805ae20b583900
Alexandre <alexandre.ferlet@hotmail.fr>
2019-12-16 17:56:53 +0100

    Changed name PlayerController -> Player + Modif TestScene

--- GravityGamejam/Assets/Scripts/Player/Player.cs
+++ GravityGamejam/Assets/Scripts/Player/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class Player : MonoBehaviour
 {
 	public float speed = 1.0f;
 	public float jumpSpeed = 10.0f;
