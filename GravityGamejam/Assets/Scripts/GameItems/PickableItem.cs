7d46aa7a1d85eb9823995a85dc4836ea9195d0b6
Vaudran <bo.vaudran.vfx@gmail.com>
2019-12-20 23:23:24 +0100

    TrackaUnstageFiles Bug ?

--- GravityGamejam/Assets/Scripts/GameItems/PickableItem.cs
+++ GravityGamejam/Assets/Scripts/GameItems/PickableItem.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PickableItem : MonoBehaviour
 {
+<<<<<<< .merge_file_a16468
 	[HideInInspector]
 	public Rigidbody rb;
 
@@ -19,11 +20,28 @@ public class PickableItem : MonoBehaviour
 	protected float drag = 0.5f;
 
 
+=======
+	public string itemName = "Item";
+	public ITEM_RARITY itemRarity = ITEM_RARITY.VERT;
+	public int weight = 0;
+	public Texture2D image;
+
+	public float hoverHeight = .2f;
+	protected Ray heightRay;
+	
+	protected Vector3 velocity;
+	protected float hDrag = .5f;
+	protected float g = 0.1f;
+	
+>>>>>>> .merge_file_a19468
 	private void Start()
 	{
 		heightRay = new Ray(transform.position, Vector3.down);
 		if (gameObject.tag != "Pickable") Debug.LogWarning("The gameobject : " + gameObject.name + " isn't tagged as Pickable, if you want to pick up this item in game you might want to tag it as 'Pickable'", gameObject);
+<<<<<<< .merge_file_a16468
 		rb = GetComponent<Rigidbody>();
+=======
+>>>>>>> .merge_file_a19468
 	}
 
 	private void OnDrawGizmos()
@@ -38,6 +56,33 @@ public class PickableItem : MonoBehaviour
 		if (Physics.Raycast(heightRay, out RaycastHit hitInfo, hoverHeight + 0.01f))
 		{
 			transform.position = hitInfo.point + Vector3.up * hoverHeight;
+<<<<<<< .merge_file_a16468
 		}
 	}
 }
+=======
+			velocity = new Vector3();
+		}
+		else
+		{
+			velocity.y += -g;
+			velocity.x *= hDrag;
+		}
+
+		transform.position += velocity;
+	}
+
+	public void SetVelocity(Vector3 newVelocity)
+	{
+		velocity = newVelocity;
+	}
+}
+
+public enum ITEM_RARITY
+{
+	VERT,
+	BLEU,
+	VIOLET,
+	JAUNE
+}
+>>>>>>> .merge_file_a19468
